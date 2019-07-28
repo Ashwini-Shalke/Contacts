020e8168adfc235a9c5392950e6bd20dfb0d95bb
Ashwini shalke <ashwinishalke050@gmail.com>
2019-07-28 21:07:16 +0530

    Implements row and section using twoDimensional Array

--- Contacts/TableView.swift
+++ Contacts/TableView.swift
@@ -13,6 +13,13 @@ class TableView : UITableViewController {
     
     let cellId = "cell"
     
+    let twoDimensionalArray = [
+        ["Ashwini","Anil","Anita","Amar"],
+        ["Babu","Birbal","Basu"],
+        ["Chirag","Chintu","Chandani","Chatur","Chetan"]
+    ]
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Contacts"
@@ -20,18 +27,24 @@ class TableView : UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
     
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = UIColor.blue
+        return label
+    }
     
-    //    override func numberOfSections(in tableView: UITableView) -> Int {
-    //        return 1
-    //    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       return  twoDimensionalArray.count
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return twoDimensionalArray[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = "Hey"
+        cell.textLabel?.text = twoDimensionalArray[indexPath.section][indexPath.row]
         return cell
         
     }
