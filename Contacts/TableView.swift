3590bb951e4b71fe893be055c58add4ebe34f6df
Ashwini shalke <ashwinishalke050@gmail.com>
2019-07-31 23:16:49 +0530

    Working on showIndexpath

--- Contacts/TableView.swift
+++ Contacts/TableView.swift
@@ -17,13 +17,24 @@ class TableView : UITableViewController {
         ["Ashwini","Anil","Anita","Amar"],
         ["Babu","Birbal","Basu"],
         ["Chirag","Chintu","Chandani","Chatur","Chetan"],
-        ["Dhiraj","Deepika","Dhanu","Dhanshree"]
+        ["Dhiraj","Deepika","Dhanu","Dhanshree"],
+        ["Manu","Monika","Mansi"]
     ]
+    
+    @objc func handleShowIndexPath() {
         
+        
+        for index in twoDimensionalArray[0].indices{
+        tableView.reloadRows(at: [indexPath], with: .left)
+        }
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        tableView.reloadRows(at: [indexPath], with: .left)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Contacts"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "ShowIndexPath", style: .plain, target: self, action: #selector(handleShowIndexPath))
         navigationController?.navigationBar.prefersLargeTitles = true
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
