2b5c64c3c2c5157ba76c4c9175b734a3de0658a8
Ashwini shalke <ashwinishalke050@gmail.com>
2019-08-07 23:18:08 +0530

    changes in fav_star

--- Contacts/ContactCell.swift
+++ Contacts/ContactCell.swift
@@ -14,14 +14,20 @@ class ContactCell : UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-       // backgroundColor = .red
+        
         let starButton = UIButton(type: .system)
-        //starButton.setTitle("Hye", for: .normal)
         starButton.setImage(UIImage(imageLiteralResourceName: "fav_star"), for: .normal)
         starButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         accessoryView = starButton
+        starButton.tintColor = UIColor.green
+        starButton.backgroundColor = .blue
+        starButton.titleLabel?.text = "AnilChu"
+        starButton.addTarget(self, action: #selector(handleFav), for: .touchUpInside)
     }
     
+    @objc func handleFav(){
+        print ("Working")
+    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
